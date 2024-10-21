e089230979d62feb557b0b76133537a494264412
zigholding <jianhanglan@163.com>
2024-10-21 20:20:10 +0800

    fix 1.7.4

--- src/NCFileExplorer.ts
+++ src/NCFileExplorer.ts
@@ -84,11 +84,11 @@ export class NCFileExplorer{
 	register(){
 		this.app.workspace.onLayoutReady(()=>{
 			let folder = (this.app.vault as any).getAllFolders()[0];
-			let dom = (this.file_explorer as any).createFolderDom(folder).constructor;
-			this._FolderDom_ = dom;
-			this.org_sort = dom.prototype.sort;
-			this.new_sort = chain_sort(this.org_sort);
-			this._FolderDom_.prototype.sort = this.new_sort;
+			// let dom = (this.file_explorer as any).createFolderDom(folder).constructor;
+			// this._FolderDom_ = dom;
+			// this.org_sort = dom.prototype.sort;
+			// this.new_sort = chain_sort(this.org_sort);
+			// this._FolderDom_.prototype.sort = this.new_sort;
 
 			this.getSortedFolderItems = this.file_explorer.constructor.prototype.getSortedFolderItems;
 			this.getSortedFolderItems_new = getSortedFolderItems(this.getSortedFolderItems);
