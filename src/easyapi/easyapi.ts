96550515d570368a528bc69e1e4b2763fada7a9d
ZigHolding <jianhanglan@163.com>
2026-04-25 00:58:39 +0800

    fix

--- src/easyapi/easyapi.ts
+++ src/easyapi/easyapi.ts
@@ -5,7 +5,6 @@ import { App, View, WorkspaceLeaf, TFile } from 'obsidian';
 import {dialog_suggest} from './gui/inputSuggester'
 import { dialog_multi_suggest } from './gui/inputMultiSuggester'
 import { dialog_prompt } from './gui/inputPrompt'
-import { dialog_mult_prompt } from './gui/multInputPrompt'
 import { dialog_markdown_prompt } from './gui/markdownPrompt'
 import {selectColor} from './gui/selectColor'
 import { openCardNavigator, type CardItem } from './gui/inputCardSuggester'
@@ -23,7 +22,6 @@ export class EasyAPI {
     dialog_suggest: Function
 	dialog_multi_suggest: Function
 	dialog_prompt: Function
-	dialog_mult_prompt: Function
 	dialog_markdown_prompt: Function
     dialog_cards: Function
     dialog_color: Function
@@ -41,7 +39,6 @@ export class EasyAPI {
         this.dialog_suggest = dialog_suggest;
 		this.dialog_multi_suggest = dialog_multi_suggest;
 		this.dialog_prompt = dialog_prompt;
-		this.dialog_mult_prompt = dialog_mult_prompt;
 		this.dialog_markdown_prompt = dialog_markdown_prompt;
 		this.dialog_cards = openCardNavigator;
         this.dialog_color = selectColor;
